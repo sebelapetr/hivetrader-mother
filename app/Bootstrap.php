b8209f7857f5d84cd66f52d5d8fb756afbfb3f2f
LAPTOP-BE1OQ90U\petrs <petrsebel@seznam.cz>
2022-08-16 16:27:26 +0200

    init skeleton

---
+++ app/Bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Nette\Bootstrap\Configurator;
+use Tracy\Debugger;
+
+
+class Bootstrap
+{
+
+	public static function boot(): Configurator
+	{
+		$configurator = new Configurator;
+		$appDir = dirname(__DIR__);
+        define("WWW_DIR", __DIR__.'/../www');
+        define("ROOT_DIR", __DIR__.'/../');
+        define("LOG_DIR", __DIR__.'/../log');
+
+		$configurator->enableTracy($appDir . '/log');
+
+		$configurator->setTimeZone('Europe/Prague');
+		$configurator->setTempDirectory($appDir . '/temp');
+
+		$configurator->createRobotLoader()
+			->addDirectory(__DIR__)
+			->register();
+
+		$configurator->addConfig($appDir . '/app/config/common.neon');
+        if (!isset($_SERVER["SESSIONNAME"]) || $_SERVER["SESSIONNAME"] !== "Console") { //todo check console
+            $isApi = substr($_SERVER['REQUEST_URI'], 0, 4) === '/api';
+            if ($isApi) {
+                $configurator->addConfig($appDir . '/app//config/apitte.neon');
+            }
+        }
+        $configurator->addConfig($appDir . '/app//config/local.neon');
+
+		return $configurator;
+	}
+}
