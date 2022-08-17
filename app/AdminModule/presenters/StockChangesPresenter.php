403a5ea910f06e8ce7e8e4aa7b0905bc632a4b26
LAPTOP-BE1OQ90U\petrs <petrsebel@seznam.cz>
2022-08-17 12:29:07 +0200

    fixes

--- app/AdminModule/presenters/StockChangesPresenter.php
+++ app/AdminModule/presenters/StockChangesPresenter.php
@@ -22,6 +22,7 @@ class StockChangesPresenter extends BaseAppPresenter
 
     public function actionDetail(int $id): void
     {
+        /** @var SupplierProduct|null $supplierProduct */
         $supplierProduct = $this->orm->supplierProducts->getById($id);
         if ($supplierProduct == null) {
             throw new \Exception("Supplier product with id " . $id . " not found");
@@ -35,33 +36,53 @@ class StockChangesPresenter extends BaseAppPresenter
         $this->getTemplate()->stockChangesData = $this->getStockChangesChartData($this->supplierProduct->id);
         $stockChangesDailyData = [];
         if (isset($_GET["from"]) && isset($_GET["to"]) && $_GET["from"] !== null && $_GET["to"] !== null) {
-            $from = \DateTimeImmutable::createFromFormat("Y-m-d", $_GET["from"])->setTime(0,0,0);
-            $to = \DateTimeImmutable::createFromFormat("Y-m-d", $_GET["to"])->setTime(23,59,59);
+            $from = \DateTimeImmutable::createFromFormat("Y-m-d", $_GET["from"]);
+            if ($from == false) {
+                throw new \Exception("Bad date from format");
+            }
+            $from = $from->setTime(0,0,0);
+            $to = \DateTimeImmutable::createFromFormat("Y-m-d", $_GET["to"]);
+            if ($to == false) {
+                throw new \Exception("Bad date to format");
+            }
+            $to = $to->setTime(23,59,59);
             $stockChangesDailyData = $this->getStockChangesChartDailyData($this->supplierProduct->id, $from, $to);
         }
         $this->getTemplate()->stockChangesDailyData = $stockChangesDailyData;
     }
 
+    /**
+     * @param int $id
+     * @return array<string, int>
+     */
     private function getStockChangesChartData(int $id): array
     {
         $stockChangesData = [];
         $stockChanges = $this->orm->supplierProducts->getStockChanges($id);
-        foreach ($stockChanges as $stockChange) {
-            /** @var \DateTimeImmutable $createdAt */
-            $createdAt = $stockChange->created_at;
-            $stockChangesData[$createdAt->format('d.m.Y H:i:s')] = $stockChange->actual_quantity;
+        if ($stockChanges !== null) {
+            foreach ($stockChanges as $stockChange) {
+                /** @var \DateTimeImmutable $createdAt */
+                $createdAt = $stockChange->created_at;
+                $stockChangesData[$createdAt->format('d.m.Y H:i:s')] = intval($stockChange->actual_quantity);
+            }
         }
         return $stockChangesData;
     }
 
+    /**
+     * @param int $id
+     * @return array<string, int>
+     */
     private function getStockChangesChartDailyData(int $id, \DateTimeImmutable $from, \DateTimeImmutable $to): array
     {
         $stockChangesData = [];
         $stockChanges = $this->orm->supplierProducts->getStockChangesDaily($id, $from, $to);
-        foreach ($stockChanges as $stockChange) {
-            /** @var \DateTimeImmutable $createdAt */
-            $createdAt = $stockChange->created_at;
-            $stockChangesData[$createdAt->format('d.m.Y H:i:s')] = $stockChange->min_stock_level;
+        if ($stockChanges !== null) {
+            foreach ($stockChanges as $stockChange) {
+                /** @var \DateTimeImmutable $createdAt */
+                $createdAt = $stockChange->created_at;
+                $stockChangesData[$createdAt->format('d.m.Y H:i:s')] = intval($stockChange->min_stock_level);
+            }
         }
         return $stockChangesData;
     }
--- readme.md
+++ readme.md
@@ -4,3 +4,11 @@ HiveTrader Mother
 Requirements
 ------------
 - PHP 8.1
+
+
+Deploy
+------
+1. PHPstan
+2. Migrations
+3. Deploy
+4. Test
